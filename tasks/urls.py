0424faaa137de57093d1fecc6f6ac0ccc6775ca5
Yoiner Duran <duranyoiner@gmail.com>
2023-05-30 12:29:12 -0500

    arreglando bug de boton cancelar en detail

--- tasks/urls.py
+++ tasks/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path('tasks/create/', CreateTaskCreateView.as_view(), name='create_tasks'),
     # con eso le digo que la ulr va a ser dinamica,en este caso le pondre el id de la tarea, y le pongo asi para que django sepa y le pongo un nombre
     path('tasks/<int:pk>/', TaskDetailUpdateView.as_view(), name='task_detail'),
-    # path('tasks/<int:pk>/complete', complete_task, name='complete_task'),
-    # path('tasks/<int:pk>/delete', delete_task, name='delete_task'),
     # peticiones
     path('tasks/peticiones/', TaskViewView.as_view(), name='peticiones_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -66,9 +66,6 @@ class HomeView(TemplateView):
         context["title"] = 'Bienvenido'
         return context
 
-# def home(request):
-#   return render(request,'home.html')
-
 
 class SignupView(FormView):
     form_class = UserCreationForm
@@ -93,38 +90,6 @@ class SignupView(FormView):
         context["title"] = 'Registro'
         return context
 
-# def signup(request):
-#   if request.method == 'GET':
-#     #si el metodo es get, le paso el formulario
-#     return render(request,'signup.html',{
-#     'form': UserCreationForm
-#   })
-#   else:
-#     #comparo las contraseñas recibidas
-#     if request.POST['password1']==request.POST['password2']:
-#       # registrando usuario
-#       #esto crea un usuario, y tambien cifra la contraseña pero todavia no lo guarda en la base de datos
-#       try:
-#         user=User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
-#       #asi ya lo guarda
-#         user.save()
-#         #despues de guardar el usuario, al login (para que guarde las credenciales o cookies en el navegador, le paso a la funcion login, el request y el usuario/objeto que cree)
-#         login(request,user)
-#         #lo redirecciono a la url con ese 'name'
-#         return redirect('tasks')
-
-#       except IntegrityError:
-#         #asi más manejo errores más especificos, los de Integrity son errores con la base de datos
-#         return render(request,'signup.html',{
-#     'form': UserCreationForm,
-#     'error':'El usuario ya existe'
-#   })
-#     else:
-#       return render(request,'signup.html',{
-#     'form': UserCreationForm,
-#     'error':'La contraseña no coinciden'
-#   })
-
 
 class TastkListView(ListView):
     model = Task
@@ -145,12 +110,6 @@ class TastkListView(ListView):
         context["title"] = 'pendientes'
         return context
 
-# @login_required
-# def tasks(request):
-#   #asi me trae las tareas que sean solo del usuario actual
-#   task=Task.objects.filter(usuario=request.user,fecha_finalizacion__isnull=True)
-#   return render(request,'tasks.html',{'tasks':task,'title':'Tareas pendientes'})
-
 
 class TasksComplete(ListView):
     model = Task
@@ -170,14 +129,6 @@ class TasksComplete(ListView):
         context = super().get_context_data(**kwargs)
         context["title"] = 'completas'
         return context
-
-
-# @login_required
-# def tasks_completed(request):
-#     # asi me trae las tareas que sean solo del usuario actual
-#     task = Task.objects.filter(
-#         usuario=request.user, fecha_finalizacion__isnull=False).order_by('-fecha_finalizacion')
-#     return render(request, 'tasks.html', {'tasks': task, 'title': 'Tareas completadas'})
 
 
 class CreateTaskCreateView(CreateView):
@@ -203,29 +154,6 @@ class CreateTaskCreateView(CreateView):
         return redirect(self.success_url)
 
 
-# @login_required
-# def create_task(request):
-#     if request.method == 'GET':
-#         return render(request, 'create_task.html', {
-#             'form': TaskForm
-#         })
-#     else:
-#         try:
-#             # esto crea un formulario
-#             form = TaskForm(request.POST)
-#         # con el sabe lo guardo en la base de datos, pero con el commit en falso, no se guarda en la base de datos, sino que en la variable que le asigne nomás para prueba
-#         # new_task = form.save(commit=False)
-#         # porque el modelo necesita el usuario, y cuando el usuario esta logueado, la clase del usuario siempre esta en el request como "request.user"
-#             new_task = form.save(commit=False)
-#             new_task.usuario = request.user
-#             new_task.save()
-#             return redirect('tasks')
-#         except ValueError:
-#             return render(request, 'create_task.html', {
-#                 'form': TaskForm,
-#                 'error': 'Por favor provee un dato valido'
-#             })
-
 class TaskDetailUpdateView(UpdateView):
     # el id que recibe por la url debe llamarse pk, y eso va en el kwargs
     model = Task
@@ -249,57 +177,6 @@ class TaskDetailUpdateView(UpdateView):
         tituloTarea = context["object"].titulo
         context["title"] = f'Tarea {tituloTarea}'
         return context
-
-
-# @login_required
-# def task_detail(request, task_id):
-#     # recibe ese id o parametro de la url (task_id)
-#     if request.method == "GET":
-#         # esta funcion, le paso el modelo y el filtro o como lo llamo con el orm de object.get, y si no lo encuentra, simplemente le dice un 404, y si lo encuntra, muestra los valores, y asi no tumba el servidor por si no encuentra esa tarea con su llave primaria
-#         # al comparar el user con el request.user, que es el usuario logueado, puedo decirle que la tarea a buscar debe tener un id y ademas que el user al cual pertenece esa tarea tambien debe conincidir con el user el cual esta logueado y asi no accede a tareas de otros usuarios
-#         task = get_object_or_404(Task, pk=task_id, usuario=request.user)
-#     # crear formulario apartir de los datoss recibidos del usuario anteriormente para editarlo
-#         form = TaskForm(instance=task)
-#         return render(request, 'task_detail.html', {'task': task, 'form': form})
-#     else:
-#         try:
-#             # obtengo el objeto/registro de la tarea
-#             task = get_object_or_404(Task, pk=task_id, usuario=request.user)
-#             # creo una instacia del formulario como cuando creamos el formulario, nomas que le paso los datos actualizados y que la instancia o tarea es la que conseguimos
-#             form = TaskForm(request.POST, instance=task)
-#             form.save()
-#             return redirect('tasks')
-#         except ValueError:
-#             return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error actualizando la tarea'})
-
-
-# @login_required
-# def complete_task(request, task_id):
-#     # obtengo la tarea
-#     task = get_object_or_404(Task, pk=task_id, usuario=request.user)
-#     # si visita la pagina (o lo mando a ella)
-#     if request.method == 'POST':
-#         # con el timezone.now() le pongo la fecha de hoy, es una lib de django, y luego guardo ese valor actualizado
-#         task.fecha_finalizacion = timezone.now()
-#         task.save()
-#         return redirect('tasks_completed')
-
-
-# @login_required
-# def delete_task(request, task_id):
-#     # obtengo la tarea
-#     task = get_object_or_404(Task, pk=task_id, usuario=request.user)
-#     # si visita la pagina (o lo mando a ella)
-#     if request.method == 'POST':
-#         # si encontro la tarea, borrala
-#         task.delete()
-#         return redirect('tasks')
-
-
-# @login_required
-# def signout(request):
-#     logout(request)  # con esto cerro la sesión
-#     return redirect('home')
 
 
 class SigninView(LoginView):
